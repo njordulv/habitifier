0a74537cf246b2d6bf40fed8e84f08815f08f868
njordulv <njordulv@gmail.com>
2024-07-24 18:33:23 +0300

    UPD: transfer fonts into separate css, DEL: old fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Header } from '@/components/Header'
 import { Toaster } from '@/components/ui/sonner'
 import { siteConfig } from '@/config/site'
 import '@/styles/globals.css'
-import '@/styles/colors.css'
 
 export const metadata: Metadata = {
   title: {
