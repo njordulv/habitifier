67aece8682e47b0c0bc1fafa5d57c1562af6a577
njordulv <njordulv@gmail.com>
2024-07-25 14:20:44 +0300

    ADD: drawer for mobile dialog popup

--- components/habits/EditHabit.tsx
+++ components/habits/EditHabit.tsx
@@ -1,4 +1,5 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
+import { useMediaQuery } from '@uidotdev/usehooks'
 import { LuClipboardEdit } from 'react-icons/lu'
 import {
   Dialog,
@@ -10,6 +11,16 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 import { Button } from '@/components/ui/button'
 import { DeleteHabit } from '@/components/habits/DeleteHabit'
 
@@ -19,34 +30,64 @@ interface Props {
 }
 
 export const EditHabit: React.FC<Props> = ({ habitId, onHabitUpdate }) => {
+  const [open, setOpen] = useState(false)
   const dialogCloseRef = useRef<HTMLButtonElement>(null)
+  const isDesktop = useMediaQuery('(min-width: 768px)')
 
   const handleSuccess = () => {
     onHabitUpdate()
     dialogCloseRef.current?.click()
   }
 
+  if (isDesktop) {
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="link">
+            <LuClipboardEdit size={20} />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[380px] max-w-[90%]">
+          <DialogHeader>
+            <DialogTitle>Task Edit</DialogTitle>
+            <DialogDescription>
+              You can make changes or delete the task
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="sm:justify-start">
+            <DeleteHabit habitId={habitId} onSuccess={handleSuccess} />
+            <DialogClose ref={dialogCloseRef} asChild>
+              <Button variant="ghost">Cancel</Button>
+            </DialogClose>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    )
+  }
+
   return (
-    <Dialog>
-      <DialogTrigger asChild>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
         <Button variant="link">
           <LuClipboardEdit size={20} />
         </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[380px] max-w-[90%]">
-        <DialogHeader>
-          <DialogTitle>Task Edit</DialogTitle>
-          <DialogDescription>
-            You can make changes or delete the task
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="sm:justify-start">
-          <DeleteHabit habitId={habitId} onSuccess={handleSuccess} />
-          <DialogClose ref={dialogCloseRef} asChild>
-            <Button variant="ghost">Cancel</Button>
-          </DialogClose>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="max-w-[380px] w-full mx-auto">
+          <DrawerHeader className="text-left">
+            <DrawerTitle>Edit profile</DrawerTitle>
+            <DrawerDescription>
+              You can make changes or delete the task
+            </DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <DeleteHabit habitId={habitId} onSuccess={handleSuccess} />
+            <DrawerClose asChild>
+              <Button variant="ghost">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
   )
 }
