a0cf774311c7a772d06ef52b904920a198b6c7ea
njordulv <njordulv@gmail.com>
2026-04-01 13:25:09 +0300

    add: keep-alive api endpoint

---
+++ app/api/keep-alive/route.ts
@@ -0,0 +1,13 @@
+'use server'
+
+import { NextResponse } from 'next/server'
+import { createServerSupabaseClient } from '@/utils/supabase/client-server'
+
+export async function GET() {
+  const supabase = createServerSupabaseClient()
+  
+  // This endpoint is just to keep the server alive
+  // It doesn't need to do anything
+    await supabase.from("habits").select("id").limit(1);
+  return NextResponse.json({ ok: true });
+}
